b2ea6c73d1674b380b2aec7d7328cd3e29bb894c
th0was14 <thomas.caillard@gmail.com>
2018-06-02 17:27:11 +0200

    remove unused import

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {
-  Router,
-  Route,
-  BrowserRouter,
-  Link,
-  Switch,
-  NavLink
-} from "react-router-dom";
-import { FormContainer } from "./components/container/FormContainer";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Home } from "./components/layout/Home";
-import { Main } from "./components/layout/Main";
+import { Layout } from "./components/layout/Layout";
 
-import { LinkContainer, IndexLinkContainer } from "react-router-bootstrap";
-import { Layout } from './components/layout/Layout';
-
-ReactDOM.render(
-  <Layout/>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Layout />, document.getElementById("root"));
