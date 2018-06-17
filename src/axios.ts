02739f7bc88927b404cee7036b5da1a556037f3e
th0was14 <thomas.caillard@gmail.com>
2018-06-18 00:59:48 +0200

    add axios instance

---
+++ src/axios.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const myToken: string =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTcxLCJpZGVuIjoiMjMzMzI2MTUxOTAzMjE1NjE2IiwibWQiOnt9LCJ0cyI6MTUyNzkzNjQ1NzE5NX0.uSCGosQIvVeEM9iIUjR6y4xZe6GvSYu_uip1B1vJ-Qk";
+
+
+const instance = axios.create({
+    baseURL: "https://api.royaleapi.com",
+    timeout: 1000,
+    headers: {
+        Authorization: "Bearer " + myToken,
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      }
+  });
+
+  export default instance;
--- src/service/royalApi/index.ts
+++ src/service/royalApi/index.ts
@@ -1,32 +1,22 @@
 import { Observable } from "rx";
-import axios from "axios";
 import { mapperToPlayerInfo } from "./mapper";
 import { PlayerInfoDto } from "./typings";
 import { PlayerInfo } from "../../components/page/PlayerClash.typings";
 
-const baseUrl: string = "https://api.royaleapi.com";
-const myToken: string =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTcxLCJpZGVuIjoiMjMzMzI2MTUxOTAzMjE1NjE2IiwibWQiOnt9LCJ0cyI6MTUyNzkzNjQ1NzE5NX0.uSCGosQIvVeEM9iIUjR6y4xZe6GvSYu_uip1B1vJ-Qk";
+import axiosInstance from "../../axios";
 
-export const postComment = (body: string, postId: number) =>
-  fetch(`${baseUrl}/comments/`, {
-    method: "post",
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ body, postId })
-  }).then(res => res.json());
+// export const postComment = (body: string, postId: number) =>
+//   fetch(`${baseUrl}/comments/`, {
+//     method: "post",
+//     headers: {
+//       Accept: "application/json, text/plain, */*",
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({ body, postId })
+//   }).then(res => res.json());
 
 const fetchByTypeById = <T>(type: string, id: string): Promise<T> => {
-  const fetchCall = axios.get(`${baseUrl}/${type}/${id}`, {
-    method: "get",
-    headers: {
-      Authorization: "Bearer " + myToken,
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
-    }
-  });
+  const fetchCall = axiosInstance.get(`/${type}/${id}`);
   return fetchCall.then(response => response.data).catch(e => {
     throw e;
   });
