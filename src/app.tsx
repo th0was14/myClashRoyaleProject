f5d9124b24b42788270d1435be1324b3228762d0
th0was14 <thomas.caillard@gmail.com>
2019-05-09 20:22:34 +0200

    delete old files

--- src/app.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Layout } from "./components/layout/Layout";
-
-ReactDOM.render(<Layout />, document.getElementById("root"));
--- src/components/layout/typings.ts
+++
@@ -1,7 +0,0 @@
-export interface NavRoute {
-  id: string;
-  label: string;
-  path: string;
-  component: any;
-  exact?: boolean;
-}
